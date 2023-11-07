0585800d8c879a4c15683667a830191e6e4b2d24
Gilbert Rabut Tsurwa <38707145+GillyRabutTsurwa@users.noreply.github.com>
2023-11-06 23:54:49 -0500

    Update scheduled-run.sh

--- scripts/scheduled-run.sh
+++ scripts/scheduled-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Puis-je utiliser ce script pour demarrer automatiquement le processus de mettre à jour ce page d'accueil ?
-# code à arriver bientôt
+# code à arriver ce vendredi 10 novembre
